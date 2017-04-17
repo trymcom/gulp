04616ce1fd8848bf4c6a166c69bc52c461a2962f
leo <oooka@trym.jp>
2017-04-17 15:30:43 +0900

    オプションまとめ

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,54 @@
 /* =========================================================
  VARIABLE
 ========================================================= */
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    cssnext = require('postcss-cssnext'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create(),
-    plumber = require('gulp-plumber'),
-    notify = require('gulp-notify'),
-    documentRoot = './';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var documentRoot = './';
 
 var src = {
       'scss': documentRoot + '**/css/',
-      'css': documentRoot
+      'css' : documentRoot
     };
+
+var options = {
+
+  serve : {
+    open: 'external',
+    port: 3000,
+    ui: false,
+    server: {
+      baseDir: documentRoot
+    }
+  },
+
+  sass : {
+    errLogToConsole: true,
+    outputStyle: 'compressed'
+  },
+
+  watch : [
+    documentRoot + '**/*.html',
+    documentRoot + '**/*.php'
+  ]
+
+}
 
 /* =========================================================
  BROWSER SYNC
 ========================================================= */
-var serveOptions = {
-      open: 'external',
-      port: 3000,
-      ui: false,
-      server: {
-        baseDir: documentRoot
-      }
-    };
-
 gulp.task('serve', () => {
-  browserSync.init(serveOptions);
+  browserSync.init(options.serve);
 });
 
 /* =========================================================
  SASS
 ========================================================= */
-var sassOptions = {
-      errLogToConsole: true,
-      outputStyle: 'compressed'
-    };
-
 gulp.task('sass', () => {
   var processors = [
     cssnext({browsers: ['last 2 version']})
@@ -50,7 +59,7 @@ gulp.task('sass', () => {
       errorHandler: notify.onError('<%= error.message %>')
     }))
     .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sass(options.sass).on('error', sass.logError))
     .pipe(postcss(processors))
     .pipe(sourcemaps.write(src.css))
     .pipe(gulp.dest(src.css))
@@ -60,17 +69,12 @@ gulp.task('sass', () => {
 /* =========================================================
  WATCH
 ========================================================= */
-var watchOptions = [
-      documentRoot + '**/*.html',
-      documentRoot + '**/*.php'
-    ];
-
 gulp.task('watch', () => {
 
   // scssファイルが変更されたらsassタスクを実行
   gulp.watch(src.scss + '**/*.scss', ['sass']);
   // phpファイルとcssファイルが変更されたら、ブラウザをリロード
-  gulp.watch(watchOptions).on('change', browserSync.reload);
+  gulp.watch(options.watch).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve', 'watch']);
